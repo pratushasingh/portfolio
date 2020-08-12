8afdc74c4a7487ebd93c4431e7c2222edd4739a1
Pratusha Singh <pratushasingh2196@gmail.com>
2020-08-11 18:25:30 -0700

    Updates

---
+++ precache-manifest.7cae639e001a85a03b7cf8751db253ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2277b94f506349b2f53bb50c3de7fa7a",
+    "url": "/pratushasingh.github.io/portfolio/index.html"
+  },
+  {
+    "revision": "29ab7ebf67a227ac1e1f",
+    "url": "/pratushasingh.github.io/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bf79e0fc48d59fd198f7",
+    "url": "/pratushasingh.github.io/portfolio/static/js/2.4c736102.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pratushasingh.github.io/portfolio/static/js/2.4c736102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29ab7ebf67a227ac1e1f",
+    "url": "/pratushasingh.github.io/portfolio/static/js/main.16b0f57e.chunk.js"
+  },
+  {
+    "revision": "feec59112a35ae1279d9",
+    "url": "/pratushasingh.github.io/portfolio/static/js/runtime-main.f745518b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pratushasingh.github.io/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
